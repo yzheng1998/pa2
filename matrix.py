1f8698cec752ca617b89b7b7f08a6b572e1f64be
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2020-03-22 17:37:37 -0400

    naive multi

--- matrix.py
+++ matrix.py
@@ -1,25 +1,22 @@
 
 # matrix multiplication code from pset1
-def matrix_mult(a, b): 
-    result = [[0,0],[0,0]]
-    for i in range(2): 
-        for j in range(2): 
-            for k in range(2): 
-                result[i][j] += a[i][k] * b[k][j] 
-    
-    for i in range(2): 
-        for j in range(2): 
-            a[i][j] = result[i][j]
-    return(a)
 
-def matrix_mult_mod(a, b): 
-    result = [[0,0],[0,0]]
-    for i in range(2): 
-        for j in range(2): 
-            for k in range(2): 
-                result[i][j] += a[i][k] * b[k][j] % 65536
-    
-    for i in range(2): 
-        for j in range(2): 
-            a[i][j] = result[i][j]
-    return(a)
+
+def matrix_mult(a, b):
+    n = len(a)
+    result = [[0]*n]*n
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                result[i][j] += a[i][k] * b[k][j]
+    return(result)
+
+
+y = matrix_mult(
+    [[2, 3],
+     [2, 2]],
+
+    [[2, 5],
+     [3, 1]]
+)
+print(y)
