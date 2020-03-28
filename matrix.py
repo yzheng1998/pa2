c36c9f51af0caa2548c3e9ee209c257187fe6bb9
Cyrus Faruque <cyrusfaruque@cyruss-mbp.attlocal.net>
2020-03-28 15:37:54 -0400

    new strassen no numpy

--- matrix.py
+++ matrix.py
@@ -7,7 +7,7 @@ from time import time
 
 def matrix_mult(a, b):
     n = len(a)
-    result = np.zeros([n, n], dtype=int)
+    result = [[0 for y in range(n)] for x in range(n)]
     for i in range(n):
         for j in range(n):
             for k in range(n):
@@ -32,25 +32,65 @@ def subtract(a, b):
             result[i][j] -= b[i][j]
     return(result)
 
-
 def strassen_mult(a, b):
     n = len(a)
 
     # base case
-    if n < 996:
+    if n <= 2:
         return(matrix_mult(a, b))
     # even case
 
-    # define LETTERS
-    A = a[: int(n/2), : int(n/2)]
-    B = a[: int(n/2), int(n/2):]
-    C = a[int(n/2):, : int(n/2)]
-    D = a[int(n/2):,  int(n/2):]
+    # new define LETTERS
+    A = [[0 for y in range(int(n/2))] for x in range(int(n/2))]
+    for i in range(int(n/2)): 
+        for j in range(int(n/2)): 
+            A[i][j] = a[i][j]
 
-    E = b[: int(n/2), : int(n/2)]
-    F = b[: int(n/2), int(n/2):]
-    G = b[int(n/2):, : int(n/2)]
-    H = b[int(n/2):,  int(n/2):]
+    B = [[0 for y in range(int(n/2))] for x in range(int(n/2))]
+    for i in range(int(n/2)): 
+        for j in range(int(n/2)): 
+            B[i][j] = a[i][j + int(n/2)]
+
+    C = [[0 for y in range(int(n/2))] for x in range(int(n/2))]
+    for i in range(int(n/2)):
+        for j in range(int(n/2)):
+            C[i][j] = a[i + int(n/2)][j]
+
+    D = [[0 for y in range(int(n/2))] for x in range(int(n/2))]
+    for i in range(int(n/2)):
+        for j in range(int(n/2)):
+            D[i][j] = a[i + int(n/2)][j + int(n/2)]
+
+    E = [[0 for y in range(int(n/2))] for x in range(int(n/2))]
+    for i in range(int(n/2)): 
+        for j in range(int(n/2)): 
+            E[i][j] = b[i][j]
+
+    F = [[0 for y in range(int(n/2))] for x in range(int(n/2))]
+    for i in range(int(n/2)): 
+        for j in range(int(n/2)): 
+            F[i][j] = b[i][j + int(n/2)]
+
+    G = [[0 for y in range(int(n/2))] for x in range(int(n/2))]
+    for i in range(int(n/2)):
+        for j in range(int(n/2)):
+            G[i][j] = b[i + int(n/2)][j]
+
+    H = [[0 for y in range(int(n/2))] for x in range(int(n/2))]
+    for i in range(int(n/2)):
+        for j in range(int(n/2)):
+            H[i][j] = b[i + int(n/2)][j + int(n/2)]
+
+    # define LETTERS
+    # A = a[: int(n/2), : int(n/2)]
+    # B = a[: int(n/2), int(n/2):]
+    # C = a[int(n/2):, : int(n/2)]
+    # D = a[int(n/2):,  int(n/2):]
+
+    # E = b[: int(n/2), : int(n/2)]
+    # F = b[: int(n/2), int(n/2):]
+    # G = b[int(n/2):, : int(n/2)]
+    # H = b[int(n/2):,  int(n/2):]
 
     p_1 = strassen_mult(A, subtract(F, H))
     p_2 = strassen_mult(add(A, B), H)
@@ -68,31 +108,58 @@ def strassen_mult(a, b):
     q_4 = subtract(subtract(add(p_5, p_1), p_3), p_7)
 
     # Allocate zeros to C
-    result = np.zeros((n, n)).astype('int')
+    result = [[0 for y in range(n)] for x in range(n)]
 
     # Replace C
-    result[0:int(n/2), 0:int(n/2)] = deepcopy(q_1)
-    result[0:int(n/2), int(n/2):] = deepcopy(q_2)
-    result[int(n/2):, 0:int(n/2)] = deepcopy(q_3)
-    result[int(n/2):, int(n/2):] = deepcopy(q_4)
+    for i in range(int(n/2)):
+        for j in range(int(n/2)):
+            result[i][j] = q_1[i][j]
+    
+    for i in range(int(n/2)):
+        for j in range(int(n/2)): 
+            result[i][j + int(n/2)] = q_2[i][j]
+    
+    for i in range(int(n/2)): 
+        for j in range(int(n/2)): 
+            result[i + int(n/2)][j] = q_3[i][j]
+    
+    for i in range(int(n/2)): 
+        for j in range(int(n/2)): 
+            result[i + int(n/2)][j + int(n/2)] = q_4[i][j]
+    # result[0:int(n/2), 0:int(n/2)] = deepcopy(q_1)
+    # result[0:int(n/2), int(n/2):] = deepcopy(q_2)
+    # result[int(n/2):, 0:int(n/2)] = deepcopy(q_3)
+    # result[int(n/2):, int(n/2):] = deepcopy(q_4)
 
     return(result)
 
+# x = np.array([[2, 3, 4, 5],
+#               [2, 2, 6, 7],
+#               [3, 2, 4, 8],
+#               [1, 2, 3, 4]])
 
-x = np.array([[2, 3, 4, 5],
-              [2, 2, 6, 7],
-              [3, 2, 4, 8],
-              [1, 2, 3, 4]])
+# y = np.array([[2, 5, 7, 5],
+#               [3, 1, 12, 1],
+#               [1, 3, 2, 4],
+#               [4, 5, 6, 9]])
 
-y = np.array([[2, 5, 7, 5],
-              [3, 1, 12, 1],
-              [1, 3, 2, 4],
-              [4, 5, 6, 9]])
+x = [[2, 3, 4, 5],
+    [2, 2, 6, 7],
+    [3, 2, 4, 8],
+    [1, 2, 3, 4]]
 
+y = [[2, 5, 7, 5],
+    [3, 1, 12, 1],
+    [1, 3, 2, 4],
+    [4, 5, 6, 9]]
+
+print(strassen_mult(x, y))
 # x = np.array([[2, 3],
 #               [2, 2]])
 # y = np.array([[2, 5],
 #               [3, 1]])
+
+# print(strassen_mult(x,y))
 
 # x = np.array([[2, 3, 4],
 #               [2, 2, 6],
@@ -102,59 +169,57 @@ y = np.array([[2, 5, 7, 5],
 #               [1, 3, 2]])
 
 
-z = matrix_mult(
-    [[2, 3, 4],
-     [2, 2, 6],
-     [3, 2, 4]],
+# z = matrix_mult(
+#     [[2, 3, 4],
+#      [2, 2, 6],
+#      [3, 2, 4]],
 
-    [[2, 5, 7],
-     [3, 1, 12],
-     [1, 3, 2]]
-)
-
-# print(y)
+#     [[2, 5, 7],
+#      [3, 1, 12],
+#      [1, 3, 2]]
+# )
 
 # q_1 = np.array([[1,2], [3,4]])
 # q_2 = np.array([[5,6],[7,8]])
 # q_3 = np.array([[1,5], [8,9]])
 # q_4 = np.array([[12,6],[1,2]])
 
-pow = 3
-a = np.random.randint(0, 2, (2**pow, 2**pow))
-b = np.random.randint(0, 2, (2**pow, 2**pow))
+# pow = 3
+# a = np.random.randint(0, 2, (2**pow, 2**pow))
+# b = np.random.randint(0, 2, (2**pow, 2**pow))
 
-start = time()
-strassen_mult(a, b)
-end = time()
-print("Strassen's: ", end="")
-print(end-start)
+# start = time()
+# strassen_mult(a, b)
+# end = time()
+# print("Strassen's: ", end="")
+# print(end-start)
 
-start_2 = time()
-matrix_mult(a, b)
-end_2 = time()
-print("Traditional: ", end="")
-print(end_2 - start_2)
-
-
-# Problem 3
-
-graph = np.random.binomial(1, .02, (2**5, 2**5))
-adj = graph + graph.T - np.diag(graph.diagonal())
-print("adj\n", adj)
+# start_2 = time()
+# matrix_mult(a, b)
+# end_2 = time()
+# print("Traditional: ", end="")
+# print(end_2 - start_2)
 
 
-def trace(graph):
-    trace = 0
-    for i in range(len(adj)):
-        trace += graph[i][i]
-    return trace
+# # Problem 3
+
+# graph = np.random.binomial(1, .02, (2**5, 2**5))
+# adj = graph + graph.T - np.diag(graph.diagonal())
+# print("adj\n", adj)
 
 
-def triangles(graph):
-    return 0
+# def trace(graph):
+#     trace = 0
+#     for i in range(len(adj)):
+#         trace += graph[i][i]
+#     return trace
 
 
-print(triangles(adj))
+# def triangles(graph):
+#     return 0
+
+
+# print(triangles(adj))
 
 # print(matrix_mult(x, y))
 # print(np.dot(a, b))
