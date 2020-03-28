fc69b3d0eea8e8aec096fced84a96a90beae99b3
Yuke Zheng <yukezheng@yukes-mbp-2.lan>
2020-03-28 19:43:48 -0400

    cleaned up code

--- matrix.py
+++ matrix.py
@@ -1,6 +1,3 @@
-
-# matrix multiplication code from pset1
-import numpy as np
 import random
 from copy import deepcopy
 from time import time
@@ -38,7 +35,7 @@ def strassen_mult(a, b):
     n = len(a)
 
     # base case
-    if n <= 2:
+    if n <= 2**7:
         return(matrix_mult(a, b))
     # even case
 
@@ -83,17 +80,6 @@ def strassen_mult(a, b):
         for j in range(int(n/2)):
             H[i][j] = b[i + int(n/2)][j + int(n/2)]
 
-    # define LETTERS
-    # A = a[: int(n/2), : int(n/2)]
-    # B = a[: int(n/2), int(n/2):]
-    # C = a[int(n/2):, : int(n/2)]
-    # D = a[int(n/2):,  int(n/2):]
-
-    # E = b[: int(n/2), : int(n/2)]
-    # F = b[: int(n/2), int(n/2):]
-    # G = b[int(n/2):, : int(n/2)]
-    # H = b[int(n/2):,  int(n/2):]
-
     p_1 = strassen_mult(A, subtract(F, H))
     p_2 = strassen_mult(add(A, B), H)
     p_3 = strassen_mult(add(C, D), E)
@@ -101,7 +87,6 @@ def strassen_mult(a, b):
     p_5 = strassen_mult(add(A, D), add(E, H))
     p_6 = strassen_mult(subtract(B, D), add(G, H))
     p_7 = strassen_mult(subtract(A, C), add(E, F))
-    # print(p_1, p_2, p_3, p_4, p_5, p_6, p_7)
 
     # define q's
     q_1 = add(subtract(add(p_5, p_4), p_2), p_6)
@@ -128,95 +113,46 @@ def strassen_mult(a, b):
     for i in range(int(n/2)):
         for j in range(int(n/2)):
             result[i + int(n/2)][j + int(n/2)] = q_4[i][j]
-    # result[0:int(n/2), 0:int(n/2)] = deepcopy(q_1)
-    # result[0:int(n/2), int(n/2):] = deepcopy(q_2)
-    # result[int(n/2):, 0:int(n/2)] = deepcopy(q_3)
-    # result[int(n/2):, int(n/2):] = deepcopy(q_4)
-
     return(result)
 
-# x = np.array([[2, 3, 4, 5],
-#               [2, 2, 6, 7],
-#               [3, 2, 4, 8],
-#               [1, 2, 3, 4]])
 
-# y = np.array([[2, 5, 7, 5],
-#               [3, 1, 12, 1],
-#               [1, 3, 2, 4],
-#               [4, 5, 6, 9]])
+def randomMatrix(n):
+    matrix = [[0] * n for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = random.randint(0, 1)
+    return matrix
 
 
-x = [[2, 3, 4, 5],
-     [2, 2, 6, 7],
-     [3, 2, 4, 8],
-     [1, 2, 3, 4]]
+pow = 9
+a = randomMatrix(2**pow)
+b = randomMatrix(2**pow)
 
-y = [[2, 5, 7, 5],
-     [3, 1, 12, 1],
-     [1, 3, 2, 4],
-     [4, 5, 6, 9]]
+print("Strassen's")
+print("Traditional")
+start = time()
+strassen = strassen_mult(a, b)
+end = time()
+strassen_time = end-start
+print(strassen_time)
 
-print(strassen_mult(x, y))
-# x = np.array([[2, 3],
-#               [2, 2]])
-# y = np.array([[2, 5],
-#               [3, 1]])
+start_2 = time()
+traditional = matrix_mult(a, b)
+end_2 = time()
+traditional_time = end_2 - start_2
+print(traditional_time)
 
-# print(strassen_mult(x,y))
-
-# x = np.array([[2, 3, 4],
-#               [2, 2, 6],
-#               [3, 2, 4]])
-# y = np.array([[2, 5, 7],
-#               [3, 1, 12],
-#               [1, 3, 2]])
+print("Strassen's is faster:", strassen_time < traditional_time)
 
 
-# z = matrix_mult(
-#     [[2, 3, 4],
-#      [2, 2, 6],
-#      [3, 2, 4]],
-
-#     [[2, 5, 7],
-#      [3, 1, 12],
-#      [1, 3, 2]]
-# )
-
-# q_1 = np.array([[1,2], [3,4]])
-# q_2 = np.array([[5,6],[7,8]])
-# q_3 = np.array([[1,5], [8,9]])
-# q_4 = np.array([[12,6],[1,2]])
-
-# pow = 3
-# a = np.random.randint(0, 2, (2**pow, 2**pow))
-# b = np.random.randint(0, 2, (2**pow, 2**pow))
-
-# start = time()
-# strassen_mult(a, b)
-# end = time()
-# print("Strassen's: ", end="")
-# print(end-start)
-
-# start_2 = time()
-# matrix_mult(a, b)
-# end_2 = time()
-# print("Traditional: ", end="")
-# print(end_2 - start_2)
-
-
-# # Problem 3
-
-# adj = np.array([[0, 1, 1, 0],
-#                 [1, 0, 1, 1],
-#                 [1, 1, 0, 1],
-#                 [0, 1, 1, 0]])
+# Problem 3
 
 V = 1024
 p = .05
 
 
 def generateGraph(prob):
-    graph = np.array([[0] * V for i in range(V)])
+    graph = [[0] * V for i in range(V)]
     for i in range(V):
         for j in range(V):
             if (i < j):
@@ -272,11 +208,11 @@ def experiment():
     return results
 
 
-print(expected())
-print(experiment())
+# print(expected())
+# print(experiment())
 
 
-print("expected", expected(p))
+# print("expected", expected(p))
 
 # print(matrix_mult(x, y))
 # print(np.dot(a, b))
